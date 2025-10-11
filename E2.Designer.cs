ee08dea9aad8422a6be159843979a79f48758ea8
JOSE BARAHONA <jose.barahona7@utp.ac.pa>
2025-10-11 10:07:30 -0500

    Agregar numeros a la tabla listo. Falta ajustar la tabla para que se expanda una X cantidad de filas y despues cree una columna.

--- E2.Designer.cs
+++ E2.Designer.cs
@@ -28,32 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dvNumbers = new System.Windows.Forms.DataGridView();
+            this.dgvNumbers = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.txtNumero = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.rdbAleatorio = new System.Windows.Forms.RadioButton();
+            this.rdbManual = new System.Windows.Forms.RadioButton();
             this.label2 = new System.Windows.Forms.Label();
             this.btnGenerar = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dvNumbers)).BeginInit();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNumbers)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // dvNumbers
+            // dgvNumbers
             // 
-            this.dvNumbers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dvNumbers.Location = new System.Drawing.Point(128, 85);
-            this.dvNumbers.Name = "dvNumbers";
-            this.dvNumbers.Size = new System.Drawing.Size(535, 152);
-            this.dvNumbers.TabIndex = 0;
+            this.dgvNumbers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNumbers.Location = new System.Drawing.Point(81, 97);
+            this.dgvNumbers.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvNumbers.Name = "dgvNumbers";
+            this.dgvNumbers.ReadOnly = true;
+            this.dgvNumbers.Size = new System.Drawing.Size(713, 181);
+            this.dgvNumbers.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(323, 24);
+            this.label1.Location = new System.Drawing.Point(431, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 29);
             this.label1.TabIndex = 1;
@@ -61,51 +66,61 @@
             // 
             // txtNumero
             // 
-            this.txtNumero.Location = new System.Drawing.Point(207, 304);
+            this.txtNumero.BackColor = System.Drawing.Color.LightGray;
+            this.txtNumero.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(186, 366);
+            this.txtNumero.Margin = new System.Windows.Forms.Padding(4);
             this.txtNumero.Multiline = true;
             this.txtNumero.Name = "txtNumero";
-            this.txtNumero.Size = new System.Drawing.Size(100, 36);
+            this.txtNumero.ReadOnly = true;
+            this.txtNumero.Size = new System.Drawing.Size(132, 43);
             this.txtNumero.TabIndex = 2;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(328, 275);
+            this.groupBox1.Controls.Add(this.rdbAleatorio);
+            this.groupBox1.Controls.Add(this.rdbManual);
+            this.groupBox1.Location = new System.Drawing.Point(347, 330);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(131, 118);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(175, 145);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
-            // radioButton1
+            // rdbAleatorio
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(11, 19);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(60, 17);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Manual";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.rdbAleatorio.AutoSize = true;
+            this.rdbAleatorio.Location = new System.Drawing.Point(15, 52);
+            this.rdbAleatorio.Margin = new System.Windows.Forms.Padding(4);
+            this.rdbAleatorio.Name = "rdbAleatorio";
+            this.rdbAleatorio.Size = new System.Drawing.Size(84, 20);
+            this.rdbAleatorio.TabIndex = 1;
+            this.rdbAleatorio.TabStop = true;
+            this.rdbAleatorio.Text = "Aleatorio";
+            this.rdbAleatorio.UseVisualStyleBackColor = true;
+            this.rdbAleatorio.CheckedChanged += new System.EventHandler(this.rdbAleatorio_CheckedChanged);
             // 
-            // radioButton2
+            // rdbManual
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(11, 42);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(66, 17);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Aleatorio";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.rdbManual.AutoSize = true;
+            this.rdbManual.Location = new System.Drawing.Point(15, 23);
+            this.rdbManual.Margin = new System.Windows.Forms.Padding(4);
+            this.rdbManual.Name = "rdbManual";
+            this.rdbManual.Size = new System.Drawing.Size(71, 20);
+            this.rdbManual.TabIndex = 0;
+            this.rdbManual.TabStop = true;
+            this.rdbManual.Text = "Manual";
+            this.rdbManual.UseVisualStyleBackColor = true;
+            this.rdbManual.CheckedChanged += new System.EventHandler(this.rdbManual_CheckedChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(108, 309);
+            this.label2.Location = new System.Drawing.Point(54, 372);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(93, 25);
             this.label2.TabIndex = 4;
@@ -113,41 +128,72 @@
             // 
             // btnGenerar
             // 
-            this.btnGenerar.Location = new System.Drawing.Point(535, 288);
+            this.btnGenerar.Location = new System.Drawing.Point(623, 346);
+            this.btnGenerar.Margin = new System.Windows.Forms.Padding(4);
             this.btnGenerar.Name = "btnGenerar";
-            this.btnGenerar.Size = new System.Drawing.Size(128, 28);
+            this.btnGenerar.Size = new System.Drawing.Size(171, 34);
             this.btnGenerar.TabIndex = 5;
             this.btnGenerar.Text = "Generar";
             this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(536, 322);
+            this.btnEliminar.Location = new System.Drawing.Point(625, 388);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(128, 28);
+            this.btnEliminar.Size = new System.Drawing.Size(171, 34);
             this.btnEliminar.TabIndex = 6;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(54, 303);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 50);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cantidad a \r\ningresar:";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.BackColor = System.Drawing.Color.LightGray;
+            this.txtCantidad.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCantidad.Location = new System.Drawing.Point(184, 310);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCantidad.Multiline = true;
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.ReadOnly = true;
+            this.txtCantidad.Size = new System.Drawing.Size(132, 43);
+            this.txtCantidad.TabIndex = 8;
             // 
             // E2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Pink;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(844, 554);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.btnGenerar);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.txtNumero);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dvNumbers);
+            this.Controls.Add(this.dgvNumbers);
+            this.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "E2";
             this.Text = "E2";
-            ((System.ComponentModel.ISupportInitialize)(this.dvNumbers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNumbers)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -157,14 +203,16 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dvNumbers;
+        private System.Windows.Forms.DataGridView dgvNumbers;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtNumero;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton rdbAleatorio;
+        private System.Windows.Forms.RadioButton rdbManual;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnGenerar;
         private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCantidad;
     }
 }
--- E2.cs
+++ E2.cs
@@ -8,18 +8,107 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Laboratorio3
 {
     public partial class E2 : Form
     {
+        private List<int> numeros = new List<int>();
+        private Random random = new Random();
+        private const int maximoFila = 7;
+
         public E2()
         {
             InitializeComponent();
         }
 
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        private void btnEliminar_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void ActualizarGrid()
+        {
+            int totalNumeros = numeros.Count;
+            int columnasNecesarias = (int)Math.Ceiling((double)totalNumeros / maximoFila);
+            for(int c = 0; c < columnasNecesarias; c++)
+            {
+                dgvNumbers.Columns.Add($"Columna{c}", $"Col {c + 1}");
+            }
+            for(int fila = 0; fila < maximoFila; fila++)
+            {
+                dgvNumbers.Rows.Add();
+            }
+            int index = 0;
+            for (int col = 0; col < columnasNecesarias; col++)
+            {
+                for (int fila = 0; fila < maximoFila; fila++)
+                {
+                    if(index < totalNumeros)
+                    {
+                        dgvNumbers.Rows[fila].Cells[col].Value = numeros[index];
+                        index++;
+                    }
+                    else { break; }
+                }
+            }
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            if (rdbAleatorio.Checked)
+            {
+                if(int.TryParse(txtCantidad.Text,out int cantidad) && cantidad > 0)
+                {
+                    numeros.Clear();
+                    for (int i =0; i <cantidad; i++)
+                    {
+                        numeros.Add(random.Next(1,101));
+                    }
+                    ActualizarGrid();
+                    txtCantidad.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, ingrese una cantidad de numeros aleatorios a ser ingresados", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                if(int.TryParse(txtNumero.Text, out int numero))
+                {
+                    numeros.Add(numero);
+                    txtNumero.Clear();
+                    txtNumero.Focus();
+                    ActualizarGrid();
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, ingrese un numero valido", "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void rdbManual_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbManual.Checked)
+            {
+                txtNumero.ReadOnly = false;
+                txtNumero.BackColor = Color.White;
+                txtCantidad.ReadOnly = true;
+                txtCantidad.BackColor = Color.LightGray;
+            }
+        }
+
+        private void rdbAleatorio_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdbAleatorio.Checked)
+            {
+                txtNumero.ReadOnly = true;
+                txtNumero.BackColor = Color.LightGray;
+                txtCantidad.ReadOnly = false;
+                txtCantidad.BackColor = Color.White;
+            }
         }
     }
 }
