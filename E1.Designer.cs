0198299d8f1088ea304d1749003338a7514f07ac
Francesco <francesco.iemma@utp.ac.pa>
2025-10-12 00:03:48 -0500

    Diseño Ejercicio 1 Modificado

--- E1.Designer.cs
+++ E1.Designer.cs
@@ -55,6 +55,7 @@
             // 
             // txtMonto
             // 
+            this.txtMonto.BackColor = System.Drawing.Color.AliceBlue;
             this.txtMonto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtMonto.Location = new System.Drawing.Point(201, 144);
             this.txtMonto.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -65,6 +66,7 @@
             // 
             // txtCliente
             // 
+            this.txtCliente.BackColor = System.Drawing.Color.AliceBlue;
             this.txtCliente.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtCliente.Location = new System.Drawing.Point(201, 53);
             this.txtCliente.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -75,9 +77,11 @@
             // 
             // btnAbrirCuenta
             // 
-            this.btnAbrirCuenta.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnAbrirCuenta.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnAbrirCuenta.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnAbrirCuenta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAbrirCuenta.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrirCuenta.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAbrirCuenta.ForeColor = System.Drawing.Color.White;
             this.btnAbrirCuenta.Location = new System.Drawing.Point(456, 47);
             this.btnAbrirCuenta.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnAbrirCuenta.Name = "btnAbrirCuenta";
@@ -90,34 +94,37 @@
             // lblMontoInicial
             // 
             this.lblMontoInicial.AutoSize = true;
-            this.lblMontoInicial.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMontoInicial.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMontoInicial.ForeColor = System.Drawing.Color.Black;
             this.lblMontoInicial.Location = new System.Drawing.Point(63, 150);
             this.lblMontoInicial.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMontoInicial.Name = "lblMontoInicial";
-            this.lblMontoInicial.Size = new System.Drawing.Size(111, 16);
+            this.lblMontoInicial.Size = new System.Drawing.Size(94, 17);
             this.lblMontoInicial.TabIndex = 1;
             this.lblMontoInicial.Text = "Monto Inicial:";
             // 
             // lblCliente
             // 
             this.lblCliente.AutoSize = true;
-            this.lblCliente.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.ForeColor = System.Drawing.Color.Black;
             this.lblCliente.Location = new System.Drawing.Point(110, 55);
             this.lblCliente.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCliente.Name = "lblCliente";
-            this.lblCliente.Size = new System.Drawing.Size(64, 16);
+            this.lblCliente.Size = new System.Drawing.Size(55, 17);
             this.lblCliente.TabIndex = 0;
             this.lblCliente.Text = "Cliente:";
             // 
             // groupBox1
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.BackColor = System.Drawing.Color.White;
             this.groupBox1.Controls.Add(this.lblCliente);
             this.groupBox1.Controls.Add(this.txtMonto);
             this.groupBox1.Controls.Add(this.lblMontoInicial);
             this.groupBox1.Controls.Add(this.txtCliente);
             this.groupBox1.Controls.Add(this.btnAbrirCuenta);
             this.groupBox1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Navy;
             this.groupBox1.Location = new System.Drawing.Point(15, 22);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox1.Name = "groupBox1";
@@ -130,42 +137,52 @@
             // rbDepositos
             // 
             this.rbDepositos.AutoSize = true;
-            this.rbDepositos.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDepositos.BackColor = System.Drawing.Color.SteelBlue;
+            this.rbDepositos.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rbDepositos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbDepositos.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbDepositos.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.rbDepositos.Location = new System.Drawing.Point(177, 61);
             this.rbDepositos.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.rbDepositos.Name = "rbDepositos";
-            this.rbDepositos.Size = new System.Drawing.Size(89, 19);
+            this.rbDepositos.Size = new System.Drawing.Size(85, 21);
             this.rbDepositos.TabIndex = 15;
             this.rbDepositos.TabStop = true;
             this.rbDepositos.Text = "Depositar";
-            this.rbDepositos.UseVisualStyleBackColor = true;
+            this.rbDepositos.UseVisualStyleBackColor = false;
             // 
             // rbRetiros
             // 
             this.rbRetiros.AutoSize = true;
-            this.rbRetiros.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbRetiros.BackColor = System.Drawing.Color.SteelBlue;
+            this.rbRetiros.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rbRetiros.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rbRetiros.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbRetiros.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.rbRetiros.Location = new System.Drawing.Point(398, 61);
             this.rbRetiros.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.rbRetiros.Name = "rbRetiros";
-            this.rbRetiros.Size = new System.Drawing.Size(75, 19);
+            this.rbRetiros.Size = new System.Drawing.Size(66, 21);
             this.rbRetiros.TabIndex = 14;
             this.rbRetiros.TabStop = true;
             this.rbRetiros.Text = "Retirar";
-            this.rbRetiros.UseVisualStyleBackColor = true;
+            this.rbRetiros.UseVisualStyleBackColor = false;
             // 
             // lblSaldoActual
             // 
             this.lblSaldoActual.AutoSize = true;
-            this.lblSaldoActual.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldoActual.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldoActual.ForeColor = System.Drawing.Color.Black;
             this.lblSaldoActual.Location = new System.Drawing.Point(28, 101);
             this.lblSaldoActual.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSaldoActual.Name = "lblSaldoActual";
-            this.lblSaldoActual.Size = new System.Drawing.Size(94, 16);
+            this.lblSaldoActual.Size = new System.Drawing.Size(81, 17);
             this.lblSaldoActual.TabIndex = 11;
             this.lblSaldoActual.Text = "SaldoActual";
             // 
             // txtSaldoActual
             // 
+            this.txtSaldoActual.BackColor = System.Drawing.Color.AliceBlue;
             this.txtSaldoActual.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtSaldoActual.Enabled = false;
             this.txtSaldoActual.Location = new System.Drawing.Point(276, 100);
@@ -177,30 +194,33 @@
             // 
             // ltbDepositos
             // 
-            this.ltbDepositos.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ltbDepositos.BackColor = System.Drawing.Color.AliceBlue;
+            this.ltbDepositos.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ltbDepositos.FormattingEnabled = true;
-            this.ltbDepositos.ItemHeight = 18;
+            this.ltbDepositos.ItemHeight = 17;
             this.ltbDepositos.Location = new System.Drawing.Point(294, 178);
             this.ltbDepositos.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.ltbDepositos.Name = "ltbDepositos";
-            this.ltbDepositos.Size = new System.Drawing.Size(159, 130);
+            this.ltbDepositos.Size = new System.Drawing.Size(159, 123);
             this.ltbDepositos.TabIndex = 9;
             this.ltbDepositos.DoubleClick += new System.EventHandler(this.ltbDepositos_DoubleClick);
             // 
             // ltbRetiros
             // 
-            this.ltbRetiros.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ltbRetiros.BackColor = System.Drawing.Color.AliceBlue;
+            this.ltbRetiros.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ltbRetiros.FormattingEnabled = true;
-            this.ltbRetiros.ItemHeight = 18;
+            this.ltbRetiros.ItemHeight = 17;
             this.ltbRetiros.Location = new System.Drawing.Point(37, 178);
             this.ltbRetiros.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.ltbRetiros.Name = "ltbRetiros";
-            this.ltbRetiros.Size = new System.Drawing.Size(159, 130);
+            this.ltbRetiros.Size = new System.Drawing.Size(159, 123);
             this.ltbRetiros.TabIndex = 8;
             this.ltbRetiros.DoubleClick += new System.EventHandler(this.ltbRetiros_DoubleClick);
             // 
             // txtSaldo
             // 
+            this.txtSaldo.BackColor = System.Drawing.Color.AliceBlue;
             this.txtSaldo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtSaldo.Enabled = false;
             this.txtSaldo.Location = new System.Drawing.Point(276, 54);
@@ -212,9 +232,11 @@
             // 
             // btnNuevo
             // 
-            this.btnNuevo.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnNuevo.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnNuevo.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnNuevo.Font = new System.Drawing.Font("Georgia", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevo.ForeColor = System.Drawing.Color.White;
             this.btnNuevo.Location = new System.Drawing.Point(487, 75);
             this.btnNuevo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.btnNuevo.Name = "btnNuevo";
@@ -227,42 +249,46 @@
             // lblSaldo
             // 
             this.lblSaldo.AutoSize = true;
-            this.lblSaldo.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.ForeColor = System.Drawing.Color.Black;
             this.lblSaldo.Location = new System.Drawing.Point(19, 58);
             this.lblSaldo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSaldo.Name = "lblSaldo";
-            this.lblSaldo.Size = new System.Drawing.Size(124, 16);
+            this.lblSaldo.Size = new System.Drawing.Size(107, 17);
             this.lblSaldo.TabIndex = 5;
             this.lblSaldo.Text = "Monto Apertura";
             // 
             // lblRetiros
             // 
             this.lblRetiros.AutoSize = true;
-            this.lblRetiros.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRetiros.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRetiros.ForeColor = System.Drawing.Color.Black;
             this.lblRetiros.Location = new System.Drawing.Point(35, 147);
             this.lblRetiros.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRetiros.Name = "lblRetiros";
-            this.lblRetiros.Size = new System.Drawing.Size(59, 16);
+            this.lblRetiros.Size = new System.Drawing.Size(51, 17);
             this.lblRetiros.TabIndex = 2;
             this.lblRetiros.Text = "Retiros";
             // 
             // lblDepositos
             // 
             this.lblDepositos.AutoSize = true;
-            this.lblDepositos.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepositos.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepositos.ForeColor = System.Drawing.Color.Black;
             this.lblDepositos.Location = new System.Drawing.Point(300, 147);
             this.lblDepositos.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDepositos.Name = "lblDepositos";
-            this.lblDepositos.Size = new System.Drawing.Size(78, 16);
+            this.lblDepositos.Size = new System.Drawing.Size(70, 17);
             this.lblDepositos.TabIndex = 1;
             this.lblDepositos.Text = "Depositos";
             // 
             // groupBox2
             // 
-            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.BackColor = System.Drawing.Color.White;
             this.groupBox2.Controls.Add(this.rbRetiros);
             this.groupBox2.Controls.Add(this.rbDepositos);
             this.groupBox2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.Navy;
             this.groupBox2.Location = new System.Drawing.Point(15, 267);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox2.Name = "groupBox2";
@@ -274,7 +300,7 @@
             // 
             // groupBox3
             // 
-            this.groupBox3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox3.BackColor = System.Drawing.Color.White;
             this.groupBox3.Controls.Add(this.lblSaldoActual);
             this.groupBox3.Controls.Add(this.lblSaldo);
             this.groupBox3.Controls.Add(this.txtSaldoActual);
@@ -285,6 +311,7 @@
             this.groupBox3.Controls.Add(this.btnNuevo);
             this.groupBox3.Controls.Add(this.txtSaldo);
             this.groupBox3.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.ForeColor = System.Drawing.Color.Navy;
             this.groupBox3.Location = new System.Drawing.Point(15, 396);
             this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.groupBox3.Name = "groupBox3";
